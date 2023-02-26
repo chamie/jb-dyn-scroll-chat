2ba510cbed0cd10b9982816e9bc913e59adc706c
Nobody <nobody@example.com>
2023-02-27 02:28:07 +0300

    Fix default app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,16 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Get to the chat/i)).toBeInTheDocument();
 });
